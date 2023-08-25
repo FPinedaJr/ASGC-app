73111e193dde7ef3ebc57b1d83c2239328d1c0b6
FPJ-psu <202280110@psu.palawan.edu.ph>
2023-08-25 20:05:23 +0800

    use sum_scores in compute_weighted_average

--- main.py
+++ main.py
@@ -14,41 +14,21 @@ def sum_scores(scores: list) -> float:
 
 
 
-def compute_weighted_average(assignment_scores, quiz_scores, final_exam_score):
+def compute_weighted_average(assignment_scores: list, quiz_scores: list, final_exam_score: list) -> float:
     assignment_weight = 30
     quiz_weight = 30
     final_exam_weight = 40
 
-    # total assignment scores
-    sum_of_your_assignment_score = 0
-    sum_of_total_assignment_score = 0
-    for score in assignment_scores:
-        score = str(score)
 
-        your_score, total_score = score.split("/")
-
-        sum_of_your_assignment_score += int(your_score)
-        sum_of_total_assignment_score += int(total_score)
-
-    total_assignment_scores = sum_of_your_assignment_score / sum_of_total_assignment_score
-
-    # total quiz scores
-    sum_of_your_quiz_score = 0
-    sum_of_total_quiz_score = 0
-    for score in quiz_scores:
-        score = str(score)
-
-        your_score, total_score = score.split("/")
-
-        sum_of_your_quiz_score += int(your_score)
-        sum_of_total_quiz_score += int(total_score)
-
-    total_quiz_scores = sum_of_your_quiz_score / sum_of_total_quiz_score
+    total_assignment_scores = sum_scores(assignment_scores)
+    total_quiz_scores = sum_scores(quiz_scores)
+    total_final_exam_scores = sum_scores(final_exam_score)
 
     assignment_grade = total_assignment_scores * assignment_weight
     quiz_grade = total_quiz_scores * quiz_weight
-    final_exam_grade = final_exam_score * final_exam_weight
+    final_exam_grade = total_final_exam_scores * final_exam_weight
 
     overall_grade = assignment_grade + quiz_grade + final_exam_grade
     rounded_overall_grade = round(overall_grade, 2)
     return rounded_overall_grade
+
